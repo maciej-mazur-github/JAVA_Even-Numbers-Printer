193ac9f593ee2c5e8f80f6ff28aac29063e5ceb9
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-03-27 20:22:47 +0200

    Add printing even numbers

--- src/EvenNumberPrinter.java
+++ src/EvenNumberPrinter.java
@@ -6,4 +6,33 @@ public class EvenNumberPrinter {
             return false;
         }
     }
+
+    public static void printEvenNumbers(int start, int end) {
+        if(end < start) {
+            System.out.println("Invalid parameters");
+            return;
+        }
+
+        int i = start;
+
+        while (i <= end) {
+            if(!isEven(i)) {
+                i++;
+                continue;
+            } else {
+                System.out.println("Even number: " + i);
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printEvenNumbers(4, 10);
+        System.out.println("*********************************");
+        printEvenNumbers(4, 4);
+        System.out.println("*********************************");
+        printEvenNumbers(4, 2);
+        System.out.println("*********************************");
+        printEvenNumbers(4, 10);
+    }
 }
