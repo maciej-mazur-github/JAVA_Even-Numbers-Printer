32161194556a90ee9ffdf4d52131e8b78b630209
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-03-27 14:44:46 +0200

    Add isEven() method

---
+++ src/EvenNumberPrinter.java
@@ -0,0 +1,9 @@
+public class EvenNumberPrinter {
+    public static boolean isEven(int number) {
+        if(number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
